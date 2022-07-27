3aa9df454e5394c2326be8e6ac5d77b0108919e3
Merry1401 <merryyy457@gmail.com>
2022-07-28 00:49:51 +0300

    edit

--- NetHelp/introducere.Designer.cs
+++ NetHelp/introducere.Designer.cs
@@ -34,7 +34,6 @@ namespace NetHelp
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -81,25 +80,11 @@ namespace NetHelp
             this.label2.TabIndex = 1;
             this.label2.Text = resources.GetString("label2.Text");
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.label4.Font = new System.Drawing.Font("Segoe UI Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.ForeColor = System.Drawing.Color.MidnightBlue;
-            this.label4.Location = new System.Drawing.Point(806, 443);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(59, 38);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "-->";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
-            // 
             // introducere
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(880, 490);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
@@ -119,6 +104,5 @@ namespace NetHelp
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label4;
     }
 }
